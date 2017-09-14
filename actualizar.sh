200057eef469cdebc3950a5b2bce1f7293be7d9a
Jesús Abad <jesusabad@users.noreply.github.com>
2017-09-14 10:32:34 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # FORMATOS
 yellow='\033[1;33m'
@@ -6,24 +6,24 @@ nc='\033[0m'
 
 #REPOSITORIOS
 printf "${yellow}Actualizando repositorios...${nc}\n"
-sudo apt-get -qq update
-printf "${yellow}Repositorios actualizados.${nc}\n"
+sudo apt update
+printf "${yellow}Repositorios actualizados.${nc}\n\n"
 
 #PAQUETES
 printf "${yellow}Actualizando paquetes...${nc}\n"
-sudo apt-get -qq upgrade -y
-printf "${yellow}Paquetes actualizados.${nc}\n"
+sudo apt upgrade -y
+printf "${yellow}Paquetes actualizados.${nc}\n\n"
 
 #KERNEL
 printf "${yellow}Actualizando KERNEL...${nc}\n"
-sudo apt-get -qq dist-upgrade -y
-printf "${yellow}KERNEL actualizado.${nc}\n"
+sudo apt full-upgrade -y
+printf "${yellow}KERNEL actualizado.${nc}\n\n"
 
 #LIMPIEZA
 printf "${yellow}Limpiando el sistema...${nc}\n"
-sudo apt-get -qq autoclean
-sudo apt-get -qq autoremove -y
-sudo apt-get -qq clean
+sudo apt autoclean
+sudo apt autoremove -y
+sudo apt clean
 
 printf "${yellow}Sistema actualizado y listo.${nc}\n"
 
