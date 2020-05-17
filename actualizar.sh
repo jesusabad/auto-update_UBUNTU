99d432534fa604647d2caa61b2c1deda617e9f45
Jesús Abad <jesusabad@users.noreply.github.com>
2020-05-17 22:11:03 +0000

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -8,7 +8,7 @@ nc='\033[0m'
 #REPOSITORIOS
 printf "${yellow}Actualizando repositorios...${nc}\n"
 sudo apt update
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
 	printf "${yellow}Repositorios actualizados.${nc}\n\n";
 	else printf "${red}Error. Inténtalo más tarde.${nc}\n" && exit;
 fi
@@ -16,7 +16,7 @@ fi
 #PAQUETES
 printf "${yellow}Actualizando paquetes...${nc}\n"
 sudo apt upgrade -y
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
 	printf "${yellow}Paquetes actualizados.${nc}\n\n";
 	else printf "${red}Error. Inténtalo más tarde${nc}\n" && exit;
 fi
@@ -24,7 +24,7 @@ fi
 #KERNEL
 printf "${yellow}Comprobando grandes actualizaciones${nc}\n"
 sudo apt full-upgrade -y
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
 	printf "${yellow}Grandes actualizaciones comprobadas${nc}\n\n";
 	else printf "${red}Error${nc}\n" && exit;
 fi
@@ -33,7 +33,7 @@ fi
 printf "${yellow}Comprobando paquetes rotos${nc}\n"
 sudo apt install -f -y
 sudo dpkg --configure -a
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
 	printf "${yellow}Paquetes rotos comprobados${nc}\n\n";
 	else printf "${red}Error${nc}\n" && exit;
 fi
@@ -43,7 +43,7 @@ printf "${yellow}Limpiando el sistema...${nc}\n"
 sudo apt autoclean
 sudo apt autoremove -y
 sudo apt clean
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
 	printf "${yellow}Limpieza terminada${nc}\n\n" && exit;
 	else printf "${red}Error${nc}\n" && exit;
 fi
