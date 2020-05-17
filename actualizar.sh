af94f6a84fc92b4bca489c41a0ca8ed7299c0437
Jesús Abad <jesusabad@users.noreply.github.com>
2020-05-17 21:46:00 +0000

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -2,36 +2,48 @@
 
 # FORMATOS
 yellow='\033[1;33m'
+red='\033[31m'
 nc='\033[0m'
 
 #REPOSITORIOS
 printf "${yellow}Actualizando repositorios...${nc}\n"
 sudo apt update
-printf "${yellow}Repositorios actualizados.${nc}\n\n"
+if [ $? = 0 ]; then
+	printf "${yellow}Repositorios actualizados.${nc}\n\n";
+	else printf "${red}Error${nc}\n" &&	exit;
+fi
 
 #PAQUETES
 printf "${yellow}Actualizando paquetes...${nc}\n"
 sudo apt upgrade -y
-printf "${yellow}Paquetes actualizados.${nc}\n\n"
+if [ $? = 0 ]; then
+	printf "${yellow}Paquetes actualizados.${nc}\n\n";
+	else printf "${red}Error${nc}\n" &&	exit;
+fi
 
 #KERNEL
-printf "${yellow}Actualizando KERNEL...${nc}\n"
+printf "${yellow}Comprobando grandes actualizaciones${nc}\n"
 sudo apt full-upgrade -y
-printf "${yellow}KERNEL actualizado.${nc}\n\n"
+if [ $? = 0 ]; then
+	printf "${yellow}Grandes actualizaciones comprobadas${nc}\n\n";
+	else printf "${red}Error${nc}\n" &&	exit;
+fi
 
 #REPARACIONES
-printf "${yellow}Busqueda y reparación de paquetes rotos${nc}\n"
+printf "${yellow}Comprobando paquetes rotos${nc}\n"
 sudo apt install -f -y
 sudo dpkg --configure -a
-printf "${yellow}Reparaciones terminadas${nc}\n\n"
+if [ $? = 0 ]; then
+	printf "${yellow}Paquetes rotos comprobados${nc}\n\n";
+	else printf "${red}Error${nc}\n" &&	exit;
+fi
 
 #LIMPIEZA
 printf "${yellow}Limpiando el sistema...${nc}\n"
 sudo apt autoclean
 sudo apt autoremove -y
 sudo apt clean
-printf "${yellow}Limpieza terminada.${nc}\n\n"
-
-printf "${yellow}Sistema actualizado y listo.${nc}\n"
-
-exit
+if [ $? = 0 ]; then
+	printf "${yellow}Limpieza terminada${nc}\n\n" && exit;
+	else printf "${red}Error${nc}\n" &&	exit;
+fi
