9849a84aa4f8c14bf79a7432f2a541024c0e6469
Jesús Abad <jesusabad@users.noreply.github.com>
2020-05-18 07:30:25 +0000

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,49 +1,50 @@
 #!/bin/sh
 
-# FORMATOS
+# VARIABLES Y FORMATOS
+var_correcto=''
 yellow='\033[1;33m'
 red='\033[31m'
 nc='\033[0m'
 
+# MENSAJE DE SALIDA
+mensaje (){
+	if [ $? -eq 0 ]; then 
+		printf "${yellow}$var_correcto${nc}\n\n";
+		else printf "${red}\nError. Inténtalo más tarde.\n\n" && exit;
+	fi
+}
+
 #REPOSITORIOS
 printf "${yellow}Actualizando repositorios...${nc}\n"
+var_correcto="Repositorios actualizados."
 sudo apt update
-if [ $? -eq 0 ]; then
-	printf "${yellow}Repositorios actualizados.${nc}\n\n";
-	else printf "${red}Error. Inténtalo más tarde.${nc}\n" && exit;
-fi
+mensaje
 
 #PAQUETES
 printf "${yellow}Actualizando paquetes...${nc}\n"
+var_correcto="Paquetes actualizados."
 sudo apt upgrade -y
-if [ $? -eq 0 ]; then
-	printf "${yellow}Paquetes actualizados.${nc}\n\n";
-	else printf "${red}Error. Inténtalo más tarde${nc}\n" && exit;
-fi
+mensaje
 
 #KERNEL
-printf "${yellow}Comprobando grandes actualizaciones${nc}\n"
+printf "${yellow}Comprobando actualización del kernel...${nc}\n"
+var_correcto="Actualización del kernel comprobada."
 sudo apt full-upgrade -y
-if [ $? -eq 0 ]; then
-	printf "${yellow}Grandes actualizaciones comprobadas${nc}\n\n";
-	else printf "${red}Error${nc}\n" && exit;
-fi
+mensaje
 
 #REPARACIONES
 printf "${yellow}Comprobando paquetes rotos${nc}\n"
-sudo apt install -f -y
+var_correcto="Paquetes rotos comprobados."
+sudo apt install -f -y &&
 sudo dpkg --configure -a
-if [ $? -eq 0 ]; then
-	printf "${yellow}Paquetes rotos comprobados${nc}\n\n";
-	else printf "${red}Error${nc}\n" && exit;
-fi
+mensaje
 
 #LIMPIEZA
 printf "${yellow}Limpiando el sistema...${nc}\n"
-sudo apt autoclean
-sudo apt autoremove -y
+var_correcto="Limpieza terminada\n\nSistema actualizado y limpio"
+sudo apt autoclean &&
+sudo apt autoremove -y &&
 sudo apt clean
-if [ $? -eq 0 ]; then
-	printf "${yellow}Limpieza terminada${nc}\n\n" && exit;
-	else printf "${red}Error${nc}\n" && exit;
-fi
+mensaje
+
+exit
